3b83251160a296ca74f717d98f53fc2d90462ba6
Wes Todd <wes@wesleytodd.com>
2014-01-30 00:02:31 -0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,60 @@
+// This plugin just does some validation
+module.exports = function(rufio) {
+
+	// Validate config, analytics key required
+	rufio.config.validate('analytics', function(val, done) {
+
+		// The analytics key is require
+		if (typeof val === 'undefined') {
+			return done([
+				'The Google Analytics plugin requires configuration.',
+				'Your `rufio.json` file should contain something like this:',
+				'{',
+					'\t"analytics": {',
+						'\t\t"code": "UA-XXXXXXXX-1"'
+					'\t}',
+				'}',
+			].join('\n'));
+		}
+		
+		// No errors
+		done();
+	});
+
+	// Analytics code is required
+	rufio.config.validate('analytics:code', function(val, done) {
+
+		if (typeof val === 'undefined' || val == '') {
+			return done([
+				'The Google Analytics plugin requires a code.',
+				'Your `rufio.json` file should contain something like this:',
+				'{',
+					'\t"analytics": {',
+						'\t\t"code": "UA-XXXXXXXX-1"'
+					'\t}',
+				'}',
+			].join('\n'));
+		}
+
+		// No errors
+		done();
+	});
+
+	// Hostname is required
+	rufio.config.validate('hostname', function(val, done) {
+
+		if (typeof val === 'undefined' || val == '') {
+			return done([
+				'The Google Analytics plugin requires a hostname.',
+				'Your `rufio.json` file should contain something like this:',
+				'{',
+					'\t"hostname": "my-site.com"'
+				'}',
+			].join('\n'));
+		}
+
+		// No errors
+		done();
+	});
+
+};
